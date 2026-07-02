f36662ff0123fa2974076dc25ec9520095cfe37a
Taylan Dogan <git@taylandogan.info>
2026-07-02 17:16:30 +0200

    feat(cmd): add kansou stats command
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- cmd/root.go
+++ cmd/root.go
@@ -160,6 +160,7 @@ scoring session, and publishes the final weighted score back to AniList.`,
 	rootCmd.AddCommand(app.scoreCmd())
 	rootCmd.AddCommand(app.serveCmd())
 	rootCmd.AddCommand(app.dbCmd())
+	rootCmd.AddCommand(app.statsCmd())
 
 	exitCode := 0
 	if err := rootCmd.Execute(); err != nil {
